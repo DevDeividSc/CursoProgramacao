e1509f9edde7d7216d3e6e311b387b8a90f3bae6
DevDeividSc <141359054+DevDeividSc@users.noreply.github.com>
2024-09-07 13:05:13 -0300

    Add files via upload
    Arquivos da ImersãoDev Alura, realizado entre os dias 02/09/2024 a 06/09/2024. O intenso utilizou a apreensão de conhecimentos de HTML, CSS, JavaScript em conjunto com  ferramenta de IA do Google, o Gemini.

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados = [
+    {
+    titulo : "Rebeca Andrade",
+    descricao: "É uma ginasta brasileira que conquistou o coração dos brasileiros e o mundo esportivo. Com sua força, determinação e talento inigualáveis, ela se tornou um dos maiores nomes da ginástica artística mundial. Bicampeã olímpica e recordista de medalhas brasileiras em Jogos Olímpicos, Rebeca é um símbolo de superação e inspiração para atletas e jovens de todo o país. Sua trajetória, marcada por conquistas e desafios, a consolida como um dos maiores ícones do esporte brasileiro.",
+    link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+    tags: "ginastica ginástica ouro"
+    
+}, 
+{
+    titulo : "Martine Grael",
+    descricao: "Velejadora brasileira, bicampeã olímpica nos Jogos de 2016 e 2020, ao lado de Kahena Kunze. Conhecida por sua habilidade e determinação, Martine é um exemplo de atleta de alto nível e inspiração para as novas gerações.",
+    link : "https://pt.wikipedia.org/wiki/Martine_Grael",
+    tags : "barco velas ouro"
+},
+{
+    titulo : "Aline Silva",
+    descricao: "Judoca brasileira, medalhista de bronze nos Jogos Olímpicos de 2016. Com sua técnica apurada e espírito competitivo, Aline é um dos maiores nomes do judô feminino brasileiro.",
+    link : "https://pt.wikipedia.org/wiki/Aline_Silva_(judoca)",
+    tags : "segundo 2 lugar judo luta guerreira tatame"
+}
+];
+
